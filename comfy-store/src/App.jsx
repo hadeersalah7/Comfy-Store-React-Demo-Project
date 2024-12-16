1bacb520e5c0edad810320d6afdedf0eb6fbe62a
hadeersalah7 <hadeermhmd70@gmail.com>
2024-12-16 14:21:59 +0200

    adding root folders and files

---
+++ comfy-store/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
