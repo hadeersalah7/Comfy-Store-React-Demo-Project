016c5df79fcd382df613f0896e048167d23a8d00
hadeersalah7 <hadeermhmd70@gmail.com>
2024-12-24 13:56:01 +0200

    adding the about page

--- comfy-store/src/pages/About.jsx
+++ comfy-store/src/pages/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <h1 className='text-4xl'>About</h1 >
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className='text-4xl font-bold leading-none tracking-tight'>We Love</h1>
+        <div className='stats bg-primary shadow'>
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia atque et voluptatibus sapiente! Neque laudantium cupiditate quisquam explicabo accusantium repudiandae ea aliquid sit repellendus adipisci ratione ab, voluptatem iure deleniti! Expedita ipsum, vitae sed veritatis possimus minus est nobis repudiandae.
+      </div>
+    </>
   )
 }
 
