c99d125c0a9e917f2f26e9e4bda9a7ca2edce8ee
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2024-12-30 12:22:35 +0000

    finishing the hero component

--- comfy-store/src/components/Hero.jsx
+++ comfy-store/src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import hero1 from "../assets/hero1.webp";
 import hero2 from "../assets/hero2.webp";
 import hero3 from "../assets/hero3.webp";
@@ -8,7 +9,21 @@ const Hero = () => {
   return (
     <div className="grid lg:grid-cols-2 gap-24 items-center">
       {/* Info */}
-      <div className="">Info</div>
+      <div>
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight">
+          We are changing the way people shop.
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et hic id
+          vitae aut tempora temporibus nemo velit omnis rem. Iusto dignissimos
+          repudiandae est.
+        </p>
+        <div className="mt-10">
+          <Link to="/products" className="btn btn-primary">
+            Our Products
+          </Link>
+        </div>
+      </div>
       {/* Carousel */}
       <div
         className="hidden h-[28rem] lg:carousel carousel-center p-4
