e37d9a63c4a4fb070aa3c5ad68abe196592eee54
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2025-01-14 11:50:31 +0000

    finishing the pagination logic

--- comfy-store/src/components/PaginationContainer.jsx
+++ comfy-store/src/components/PaginationContainer.jsx
@@ -5,8 +5,12 @@ const PaginationContainer = () => {
   const pages = Array.from({ length: pageCount }, (_, index) => {
     return index + 1
   })
+  const { search, pathname } = useLocation()
+  const navigate = useNavigate()
   const handlePageChange = (pageNumber) => {
-    console.log({ pageNumber })
+    const searchParams = new URLSearchParams(search)
+    searchParams.set('page', pageNumber)
+    navigate(`${pathname}?${searchParams.toString()}`)
   }
   if (pageCount < 2) return null
   return (
@@ -18,7 +22,11 @@ const PaginationContainer = () => {
         {pages.map((pageNumber) => {
           return <button
             key={pageNumber}
-            onClick={() => handlePageChange(pageNumber)}
+            onClick={() => {
+              let prevPage = page - 1
+              if(prevPage < 1) prevPage = pageCount
+              handlePageChange(prevPage)
+            }}
             className={`btn btn-xs sm:btn-md border-none join-item 
             ${pageNumber === page ? 'bg-base-300 border-base-300' : ''}`}
           >
@@ -26,7 +34,11 @@ const PaginationContainer = () => {
   </button>
 })}
         <button className="btn btn-xs sm:btn-md join-item"
-          onClick={() => handlePageChange('next')}
+          onClick={() => {
+            let nextPage = page + 1;
+            if (nextPage > pageCount) nextPage = 1;
+            handlePageChange(nextPage)
+          }}
         >Next</button>
       </div>
     </div>
