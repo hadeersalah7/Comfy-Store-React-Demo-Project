8707ecba42ef203838aafc025d724b08aa208ac4
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2025-01-14 08:42:39 +0000

    finishing the filters params

--- comfy-store/src/components/Filters.jsx
+++ comfy-store/src/components/Filters.jsx
@@ -4,7 +4,8 @@ import FormSelect from './FormSelect';
 import FormRange from './FormRange';
 import FormCheckbox from './FormCheckbox';
 const Filters = () => {
-  const { meta } = useLoaderData()
+  const { meta, params } = useLoaderData()
+  const {search, category, company, order, price, shipping} = params
   return (
     <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8
     sm:grid-cols-2 md:grid-cols-4 items-center'>
@@ -13,13 +14,16 @@ const Filters = () => {
         type="search"
         label="search product"
         name="search"
-        size="input-sm" />
+        size="input-sm"
+        defaultValue={search}
+      />
       {/* CATEGORIES */}
       <FormSelect
         label='select category'
         name='category'
         list={meta.categories}
         size='select-sm'
+        defaultValue={category}
       />
       {/* COMPANIES */}
       <FormSelect
@@ -27,6 +31,7 @@ const Filters = () => {
         name='company'
         list={meta.companies}
         size='select-sm'
+        defaultValue={company}
       />
       {/* ORDER */}
       <FormSelect
@@ -34,22 +39,25 @@ const Filters = () => {
         name='order'
         list={['a-z', 'z-a', 'high', 'low']}
         size='select-sm'
+        defaultValue={order}
       />
       {/* PRICE */}
       <FormRange
         label='select price'
         name="price"
         size="range-sm"
+        price={price}
       />
       {/* SHIPPING */}
       <FormCheckbox
         name='shipping'
         label='free shipping'
         size='checkbox-sm'
+        defaultValue={shipping}
       />
       {/* BUTTONS */}
       <button type='submit' className='btn btn-primary btn-sm'>search</button>
-      <button type="reset" className='btn btn-accent btn-sm'>reset</button>
+      <Link to="/products" className='btn btn-accent btn-sm'>reset</Link>
     </Form>
   )
 }
