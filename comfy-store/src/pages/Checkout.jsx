ae3e830add21ec5d24df49a0fdcb8838159c42d3
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2025-01-26 08:54:38 +0000

    updates

--- comfy-store/src/pages/Checkout.jsx
+++ comfy-store/src/pages/Checkout.jsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux";
+import { CheckoutForm, SectionTitle, CartTotals } from "../components";
 const Checkout = () => {
-    return (
-        <h1 className='text-4xl'>Checkout</h1 >
-    )
-}
+  const cartTotals = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotals === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid gap-8 md:grid-cols-2 items-start">
+      <CheckoutForm />
+      <CartTotals />
+      </div>
+    </>
+  );
+};
 
-export default Checkout
+export default Checkout;
--- comfy-store/src/pages/Login.jsx
+++ comfy-store/src/pages/Login.jsx
@@ -1,6 +1,42 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, redirect, useNavigate } from "react-router-dom";
 import { FormInput, SubmitBtn } from "../components";
+import { toast } from "react-toastify";
+import customFetch from "../utils";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../features/user/UserSlice";
+
+export const action = (store) => async ({request}) => {
+    const formData = await request.formData()
+    const data = Object.fromEntries(formData);
+    try {
+        const response = await customFetch.post("/auth/local", data)
+        store.dispatch(loginUser(response.data))
+        toast.success("LoggedIn successfully!")
+        return redirect("/")
+    } catch (error) {
+        const errorMessage = error?.response?.data?.error?.message || "Please double check your credentials"
+        toast.error(errorMessage)
+        return null;
+    }
+}
+
 const Login = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
+
+    const loginDemoUser = async () => {
+        try {
+            const response = await customFetch.post("/auth/local", {
+                identifier: "test@test.com",
+                password: "secret"
+            })
+            dispatch(loginUser(response.data))
+            navigate("/");
+            toast.success("Welcome guest user!");
+        } catch (error) {
+            toast.error("Failed to login user please try again!")
+        }
+    }
     return (
         <section className="h-screen grid place-items-center">
             <Form
@@ -12,18 +48,16 @@ const Login = () => {
                     type="email"
                     name="identifier"
                     label="email"
-                    defaultValue="test@test.com"
                 />
                 <FormInput
                     type="password"
                     name="password"
                     label="password"
-                    defaultValue="secret"
                 />
                 <div className="mt-4">
                     <SubmitBtn text="Login" />
                 </div>
-                <button type="button" className="btn btn-secondary btn-block">
+                <button type="button" className="btn btn-secondary btn-block" onClick={loginDemoUser}>
                     guest user
                 </button>
                 <p className="text-center">
