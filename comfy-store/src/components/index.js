aa02e4cca1b5aca9aeefed2b1cc1d76ea6ef78ff
Hadeer Salah <91800040+hadeersalah7@users.noreply.github.com>
2025-01-29 13:46:08 +0000

    adding orders list table

--- comfy-store/src/components/index.js
+++ comfy-store/src/components/index.js
@@ -14,4 +14,5 @@ export { default as Loading } from "./Loading";
 export { default as CartTotals } from "./CartTotals";
 export { default as CartItemsList } from "./CartItemsList";
 export { default as CheckoutForm } from "./CheckoutForm";
-
+export { default as OrderList } from "./OrderList";
+export { default as ComplexPaginationContainer } from "./ComplexPaginationContainer";
